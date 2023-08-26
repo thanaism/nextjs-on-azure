a261ec04074d8ffa3dc28ceec01f6cb1e1902e87
thanaism <62784012+thanaism@users.noreply.github.com>
2023-08-27 02:58:43 +0900

    Revert "add: 通常のapi routeを試験的に追加"
    This reverts commit f241815b936572047654e410fb04ba6add6ee506.

--- src/pages/api/time.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    res.status(200).json({ time: new Date() });
-}
